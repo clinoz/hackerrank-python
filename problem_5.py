e58738e5a204091bb00d840c4df2d3cb32feca5a
cosmian <softkey95@gmail.com>
2019-10-24 19:39:57 +0900

    problem 5 (Find the Runner-Up Score) updated

---
+++ problem_5.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    winner = max(arr)
+    i = 0
+    while i<n:
+        if winner == max(arr):
+            arr.remove(max(arr))
+        i = i+1
+    print(max(arr))
